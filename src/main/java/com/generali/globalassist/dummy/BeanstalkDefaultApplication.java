ac1beb045a287af38a20ae60fe8ebacd91bf11ed
phoeniix <phoeniix@gmail.com>
2020-03-27 10:42:28 -0700

    adding new dummy service for initial terraformed beanstalk apps

---
+++ src/main/java/com/generali/globalassist/dummy/BeanstalkDefaultApplication.java
@@ -0,0 +1,13 @@
+package com.generali.globalassist.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalkDefaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanstalkDefaultApplication.class, args);
+	}
+
+}
